385f27ab248ea45dbf126c0be82cf4a3c0697243
Emrys5 <9097206@qq.com>
2016-09-08 15:45:19 +0800

    frist summary

---
+++ Default.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="UploadImg" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>图片上传</title>
+    <script src="Scripts/ueditor/third-party/jquery-1.10.2.js"></script>
+    <script src="Scripts/ueditor/ueditor.config.js"></script>
+    <script src="Scripts/ueditor/ueditor.all.js"></script>
+    <script src="Scripts/ueditor/custom.js"></script>
+    <script type="text/javascript">
+        function callbackImg(imgName) {
+            var names = imgName.split(",");
+            for (var i = 0; i < names.length; i++) {
+                $("#show-img-name").append($("<div></div>").html($("<a></a>").html(names[i]).attr("href", "/upload/image/" + names[i] + "").attr("target", "_blank")));
+            }
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <a href="javascript:uploadImgs()">多图片上传</a>
+            <a href="javascript:uploadImg()">单图片上传</a>
+            <a href="javascript:insertScrawl()">涂鸦</a>
+        </div>
+        <div id="loading"></div>
+        <div id="show-img-name"></div>
+    </form>
+</body>
+</html>
---
+++ Scripts/ueditor/custom.js
@@ -0,0 +1,61 @@
+﻿/*!
+ * UEditor-Upload
+ * version: Emrys
+ * build: 2014年10月22日 10:19:26 (中国标准时间)
+ */
+$(function () {
+    $("body").append(" <div id=\"editor-img\" style=\"display:none\"></div><div id=\"temp-img-list\" style=\"display: none\"></div>");
+    var ue = UE.getEditor('editor-img');
+    //监听编辑器文本内容变化
+    UE.getEditor('editor-img').addListener('contentChange', function (editor) {
+        //获取编辑器中的内容（html 代码）
+        var img = UE.getEditor('editor-img').getPlainTxt();
+        if (img != "") {
+            //判断是否是单图片上传，如果是单传不做任何处理，等待回调函数再次调用。
+            if (img.indexOf("loadingclass") == -1) {
+                //把编辑器中的内容放到临时div中，然后进行获取文件名称。
+                $("#temp-img-list").html(img);
+                var array = new Array();
+                //循环获取文件名称
+                $("#temp-img-list img").each(function () {
+                    var src = $(this).attr("src");
+                    var name = src.replace("/upload/image/", "");
+                    array.push(name);
+                });
+                //清空编辑器中的内容，以便下一次添加图片。
+                UE.getEditor('editor-img').execCommand('cleardoc');
+                //调用callbackImg获取懂图片名称
+                if (typeof callbackImg === "function") {
+                    eval("callbackImg('" + array + "')");
+                }
+            }
+        }
+    });
+
+});
+//单传图片开始上传,显示等待。
+function preUploadSingleImg() {
+    if ($("#loading").length > 0) {
+        $("#loading").html("<img src='/Scripts/ueditor/loading.gif'>");;
+    }
+
+}
+
+//单传图片回调,然后清理内容,在清理内容以后会自动调用contentChange事件，然后再获取上传文件的文件名。
+function uploadSingleImgCallback() {
+    UE.getEditor('editor-img').execCommand('cleardoc');
+    if ($("#loading").length > 0) {
+        $("#loading").empty();
+    }
+}
+
+function uploadImg() {
+    $(".edui-for-simpleupload input").click();
+}
+
+function uploadImgs() {
+    $(".edui-for-insertimage .edui-button-body")[0].click();
+}
+function insertScrawl() {
+    $('.edui-for-scrawl .edui-button-body')[0].click();
+}
